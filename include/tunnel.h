01b7441688353e9a3862399906d79f4617e45ebd
JeremyLetang <letang.jeremy@gmail.com>
2015-02-22 20:00:17 +0100

    basic impl

--- include/tunnel.h
+++ include/tunnel.h
@@ -19,3 +19,110 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
+
+#ifndef TUNNEL
+#define TUNNEL
+
+#include <tuple>
+#include <memory>
+
+#include "mpsc_queue/include/mpsc_queue.h"
+
+namespace tunnel {
+
+template <typename T>
+struct recv_result {
+    T value;
+    bool state;
+};
+
+template <typename T>
+struct send_error {
+    T value;
+    bool state;
+};
+
+template <class T>
+struct port;
+
+template <class T>
+struct chan {
+
+private:
+
+    std::shared_ptr<mpsc::queue<T>> queue;
+
+    chan(std::shared_ptr<mpsc::queue<T>> q)
+    : queue(q) {}
+
+
+public:
+
+    chan(chan& oth) = delete;
+    chan(const chan& oth) = delete;
+
+    chan(chan&& oth)
+    : queue(std::move(oth.queue)) {
+        oth.queue = nullptr;
+    }
+
+    auto clone() -> chan {
+        auto new_chan = chan(this->queue);
+        return std::move(new_chan);
+    }
+
+    auto send(T&& t) -> send_error<T>  {
+        this->queue->push(std::move(t));
+        return send_error<T> {T(), true};
+    }
+
+    template <typename U>
+    friend auto make() -> std::tuple<chan<U>, port<U>>;
+
+};
+
+template <class T>
+struct port {
+
+private:
+
+    std::shared_ptr<mpsc::queue<T>> queue;
+
+    port(std::shared_ptr<mpsc::queue<T>> q)
+    : queue(q) {}
+
+
+public:
+
+    port(port& oth) = delete;
+    port(const port& oth) = delete;
+
+    port(port&& oth) {
+        this->queue = oth.queue;
+        oth.queue = nullptr;
+    }
+
+    auto recv() -> recv_result<T> {
+        if (this->queue->is_empty()) {
+            return recv_result<T> {T(), false};
+        }
+        return recv_result<T> {this->queue->pop().value, true};
+    }
+
+    template <typename U>
+    friend auto make() -> std::tuple<chan<U>, port<U>>;
+
+
+};
+
+template <class T>
+auto make() -> std::tuple<chan<T>, port<T>> {
+    auto q = std::make_shared<mpsc::queue<T>>();
+    auto c = chan<T>(q);
+    auto p = port<T>(q);
+    return std::move(std::make_tuple(std::move(c), std::move(p)));
+}
+
+}
+
+#endif
--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -15,5 +15,18 @@ std::ostream& operator<<(std::ostream& os, const point& p) {
 
 int
 main() {
+    std::cout << "comm" << std::endl;
+    auto comm = tunnel::make<point>();
+    std::cout << "chan" << std::endl;
+    auto chan = std::move(std::get<0>(comm));
+    std::cout << "port" << std::endl;
+    auto port = std::move(std::get<1>(comm));
+    std::cout << "chan_copy" << std::endl;
+    tunnel::chan<point> chan_copy = chan.clone();
+
+    std::cout << "chan_send" << std::endl;
+    chan.send(std::move(point {1, 2}));
+    std::cout << "received: " << port.recv().value << std::endl;
+
     return 0;
 }
